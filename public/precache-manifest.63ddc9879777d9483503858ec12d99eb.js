0da359d53c06ef02b71ba1b2bb3e03f8810c9c4f
AnubhavK27 <37549611+AnubhavK27@users.noreply.github.com>
2020-07-16 23:42:05 +0530

    Finished

---
+++ public/precache-manifest.63ddc9879777d9483503858ec12d99eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf310c37dd9cb87ed541c399d128e53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "163d7b2bbf6e8f00bc35",
+    "url": "/static/css/main.f79c70fe.chunk.css"
+  },
+  {
+    "revision": "8e5791cf77d6200a0c68",
+    "url": "/static/js/2.444b05d2.chunk.js"
+  },
+  {
+    "revision": "452512fa9f48054be807e37947522c62",
+    "url": "/static/js/2.444b05d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "163d7b2bbf6e8f00bc35",
+    "url": "/static/js/main.3f78d622.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "8c8aad808f4ce1ad2f2a7bf6b1e1c5fd",
+    "url": "/static/media/logo.8c8aad80.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express'),
       { graphqlHTTP } = require('express-graphql'),
       schema = require('./schema'),
-      cors = require('cors');
+      cors = require('cors'),
+      path = require('path');
 
 const app = express();
 
@@ -12,6 +13,12 @@ app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
